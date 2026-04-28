6fbc30a97a21b18fcdb6ff80098a185025962a7b
Hunter2066 <aryalaashik9@gmail.com>
2026-04-28 07:28:08 +0545

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true }, // GitHub Pages doesn't support Next.js Image Optimization
-};
-
-export default nextConfig;
-
